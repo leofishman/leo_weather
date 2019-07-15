24ce64d7d0ce13479593f7527225f1a3ec6c7f7a
leofishman <leofishman@gmail.com>
2019-07-15 08:40:26 -0300

    responsive

--- assets/js/leo_weather.js
+++ assets/js/leo_weather.js
@@ -4,7 +4,7 @@
       let lat = document.getElementById('lat').innerText;
       let lon = document.getElementById('lon').innerText;
 
-      var mymap = L.map('mapid').setView([lat, lon], 9);
+      var mymap = L.map('mapid').setView([lat, lon], 7);
 
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
